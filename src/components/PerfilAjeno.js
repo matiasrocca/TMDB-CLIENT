2f6352e84846cbe1a2c7782eef6ff4e3ae0c175b
matiasrocca <matiasrocca98@gmail.com>
2022-05-10 14:40:24 -0300

    correccion perfil ajeno

--- src/components/PerfilAjeno.js
+++ src/components/PerfilAjeno.js
@@ -4,27 +4,42 @@ import axios from "axios";
 import { useParams } from "react-router"
 import { Link } from "react-router-dom";
 import Card from "./Card";
-
+import { userSpecific } from "../services/AllUsers";
+import { favoritosUsuario } from "../services/FavoritosUsuario";
 
 const PerfilAjeno = () => {
+    const [isLoading, setLoading] = useState(true);
     const {id} = useParams()
     const [busqueda, setBusqueda] = useState({})
     const [favoritos, setFavoritos] = useState([])
 
     useEffect(()=>{
-        axios
-        .get(`/api/users/${id}`)
-        .then(resultados => resultados.data)
-        .then(res => setBusqueda(res))
 
-        axios
-        .get(`/api/users/${id}/favoritos`)
-        .then(resultados => resultados.data)
-        .then(res => setFavoritos(res))
+
+        Promise.all([
+            userSpecific(id),
+            favoritosUsuario(id)
+        ]).then(results => {
+            setBusqueda(results[0]),
+            setFavoritos(results[1])
+        }).then(()=>setLoading(false))
+
 
     },[])
 
     return (
+        <>
+        {isLoading ?
+            (
+                <div className="main2">
+                    <section className="section is-large" style={{"display":"flex", "justifyContent":"center"}}>
+                        <h1 className= "texto">
+                            De Pelicula
+                        </h1>
+                    </section>
+                </div>
+            ) : 
+            (
         <div className="main" style={favoritos[0]?({"height":"100%"}):({"height":"100vh"})}>
             <section className="section is-large" style={{"padding": "14rem 3rem 3rem 3rem"}}>
                 <div className="componentesInicio" style={{"padding":"5% 5% 5% 5%", "backgroundColor":"rgba(0, 0, 0, 0.508)", "borderRadius":"5px"}}>
@@ -45,6 +60,8 @@ const PerfilAjeno = () => {
                 </div>
             </section>
         </div>
+        )}
+    </>
     )
 
 }
--- src/services/AllUsers.js
+++ src/services/AllUsers.js
@@ -16,10 +16,22 @@ export const allUsers = async () => {
 export const usersQuery = async (query) => {
 
     try{
-        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/all/${query}`, {withCredentials:true});
+        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/all/${query}`);
 
         return res.data
     } catch (error) {
         console.log(error);
       }
 };
+
+export const userSpecific = async (id) => {
+
+    try{
+        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/:${id}`);
+
+        return res.data;
+    } catch (error) {
+        console.log(error)
+    }
+
+}
