4257a8893740686b0cb6c88215b46ac9bf2b794c
matiasrocca <matiasrocca98@gmail.com>
2022-05-10 14:16:59 -0300

    variable de entorno correcta para todos

--- src/services/FavoritosUsuario.js
+++ src/services/FavoritosUsuario.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const favoritosUsuario = async (id) => {
 
     try{
-        const results = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/${id}/favoritos`)
+        const results = await axios.get(`${process.env.REACT_APP_SERVER_URL}api/users/${id}/favoritos`)
         return results.data
     } catch (error) {
         console.log(error);
@@ -14,7 +14,7 @@ export const favoritosUsuario = async (id) => {
 export const añadirAFavoritos = async (userId, movieId, title, poster_path, type) => {
 
     try{
-        const results = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/users/${userId}/add`, {
+        const results = await axios.post(`${process.env.REACT_APP_SERVER_URL}api/users/${userId}/add`, {
             id : movieId,
             title: title,
             poster_path: poster_path,
@@ -29,7 +29,7 @@ export const añadirAFavoritos = async (userId, movieId, title, poster_path, typ
 export const eliminarDeFavoritos = async (userId, movieId) => {
 
     try{
-        const results = await axios.delete(`${process.env.REACT_APP_SERVER_URL}/api/users/${userId}/${movieId}/delete`)
+        const results = await axios.delete(`${process.env.REACT_APP_SERVER_URL}api/users/${userId}/${movieId}/delete`)
         return results
     } catch (error) {
         console.log(error);
@@ -41,7 +41,7 @@ export const esFavorita = async (usuario, peliculaId) => {
     if(usuario) usuarioId = usuario.id
 
     try{
-        const results = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/${usuarioId}/favoritos/${peliculaId}`)
+        const results = await axios.get(`${process.env.REACT_APP_SERVER_URL}api/users/${usuarioId}/favoritos/${peliculaId}`)
         return results.data.success
     } catch (error) {
         return false
